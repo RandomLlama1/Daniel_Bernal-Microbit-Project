4ce264e683caf969b8297df63b34b0b8b1dcc8f6
RandomLlama1 <77157819+RandomLlama1@users.noreply.github.com>
2021-01-09 16:17:09 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-global num = randint(1,20)
+num = randint(1,20)
  
 def on_button_pressed_a():
+    global num
     num = num-2
     basic.show_number(num)
     return num
  
 def on_button_pressed_b():
+    global num
     num = num-1
     basic.show_number(num)
     return num 
 
+def victory():
+    if num == 0:
+        basic.show_string("You win!")
+        basic.show_icon(IconNames.HEART)
+    if num < 0:
+        basic.show_string("You lose :(")
 if input.is_gesture(Gesture.Shake):
     basic.show_number(num)
-
+'''
 def on_forever():
-    input.on_button_pressed(Button.A, on_button_pressed_a)
-    input.on_button_pressed(Button.B, on_button_pressed_b)
     if num == 0:
         break
     elif num > 0:
         break
 basic.forever(on_forever)
- 
-
- 
- 
- 
+'''
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
  
 
 
--- main.ts
+++ main.ts
@@ -1,23 +1,38 @@
 let num = randint(1, 20)
-/** 
-while input.on_gesture(Gesture.Shake):
-    basic.show_number(num)
+function victory() {
+    if (num == 0) {
+        basic.showString("You win!")
+        basic.showIcon(IconNames.Heart)
+    }
+    
+    if (num < 0) {
+        basic.showString("You lose :(")
+    }
+    
+}
 
- */
 if (input.isGesture(Gesture.Shake)) {
     basic.showNumber(num)
 }
 
-basic.forever(function on_forever() {
-    
-})
+/** 
+def on_forever():
+    if num == 0:
+        break
+    elif num > 0:
+        break
+basic.forever(on_forever)
+
+ */
 input.onButtonPressed(Button.A, function on_button_pressed_a(): number {
-    let num = num - 2
+    
+    num = num - 2
     basic.showNumber(num)
     return num
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b(): number {
-    let num = num - 1
+    
+    num = num - 1
     basic.showNumber(num)
     return num
 })
