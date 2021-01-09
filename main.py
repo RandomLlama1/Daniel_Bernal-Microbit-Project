b88df0a0dc9b4e5a62e233fb04aea7771089fbe4
RandomLlama1 <77157819+RandomLlama1@users.noreply.github.com>
2021-01-09 17:14:41 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ num = randint(1,20)
 chance = randint(1,3)
 
 def on_button_pressed_ab():
+    basic.clear_screen()
+    basic.pause(10)
     global num
     global chance
     if chance == 1:
@@ -12,15 +14,19 @@ def on_button_pressed_ab():
         num = num - 5
         return num
     else: 
-        num = num + 1
+        num = num + 20
         return num
 
 def on_button_pressed_a():
+    basic.clear_screen()
+    basic.pause(10)
     global num
     num = num-2
     return num
 
 def on_button_pressed_b():
+    basic.clear_screen()
+    basic.pause(10)
     global num
     num = num-1
     return num
@@ -32,7 +38,6 @@ while True:
     input.on_button_pressed(Button.A, on_button_pressed_a)
     input.on_button_pressed(Button.B, on_button_pressed_b)
     input.on_button_pressed(Button.AB, on_button_pressed_ab)
-    basic.clear_screen()
     
     if num == 0:
         basic.clear_screen()
--- main.ts
+++ main.ts
@@ -3,16 +3,22 @@ let chance = randint(1, 3)
 while (true) {
     basic.showNumber(num)
     input.onButtonPressed(Button.A, function on_button_pressed_a(): number {
+        basic.clearScreen()
+        basic.pause(10)
         
         num = num - 2
         return num
     })
     input.onButtonPressed(Button.B, function on_button_pressed_b(): number {
+        basic.clearScreen()
+        basic.pause(10)
         
         num = num - 1
         return num
     })
     input.onButtonPressed(Button.AB, function on_button_pressed_ab(): number {
+        basic.clearScreen()
+        basic.pause(10)
         
         
         if (chance == 1) {
@@ -22,12 +28,11 @@ while (true) {
             num = num - 5
             return num
         } else {
-            num = num + 1
+            num = num + 20
             return num
         }
         
     })
-    basic.clearScreen()
     if (num == 0) {
         basic.clearScreen()
         basic.showString("You win!")
